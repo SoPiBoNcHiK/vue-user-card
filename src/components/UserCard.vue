ca3f70a3b055c6b2d8e6bc24b2378d83ef90501b
sopibonchik <sopibonchik@gmail.com>
2020-12-23 23:43:16 +0300

    Now UserCard component gets data from props

--- src/components/UserCard.vue
+++ src/components/UserCard.vue
@@ -1,23 +1,23 @@
 <template>
   <div class="user-card">
-    <img src="/img/70.jpg" class="user-photo" />
-    <p class="user-nickname">romashka</p>
+    <img v-bind:src="avatar" class="user-photo" />
+    <p class="user-nickname">{{login}}</p>
     <p class="user-name">
-      Иванов Иван<br />
-      Иванович
+      {{lastname + ' ' + firstname}}<br />
+      {{patronymic}}
     </p>
     <hr />
     <p class="user-info-item">
       <img src="/img/location.png" />
-      Москва, Юбилейная 50
+      {{adress}}
     </p>
     <p class="user-info-item">
       <img src="/img/mail.png" />
-      coldrabbit48@example.com
+      {{email}}
     </p>
     <p class="user-info-item">
       <img src="/img/phone.png" />
-      +7-495-266-57-34
+      {{phone}}
     </p>
   </div>
 </template>
@@ -25,6 +25,9 @@
 <script>
 export default {
   name: "UserCard",
+  props: [
+      'avatar', 'lastname', 'firstname', 'patronymic', 'login', 'adress', 'phone', 'email'
+  ]
 };
 </script>
 
